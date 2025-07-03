737a6c9e17a134da6bc6b9df50632d7f58905a0b
Vora-Manas <143721978+Vora-Manas@users.noreply.github.com>
2025-07-03 18:13:04 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+streamlit run app.py --server.port 8000 --server.address 0.0.0.0 --server.enableCORS false
 chmod +x startup.sh
